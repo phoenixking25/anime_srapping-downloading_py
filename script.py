222cb85042279f789608fc4867bc05184ac6833f
anuj sharma <anujjangra25119@gmail.com>
2017-10-15 16:00:29 +0530

    catching link

---
+++ script.py
@@ -0,0 +1,46 @@
+from bs4 import BeautifulSoup
+import requests
+import click
+import re
+
+
+urls = [
+    'http://crusader.pp.ua/anime/',
+    'http://booksaz.space/s1/Anime/',
+    'https://archives.eyrie.org/anime/',
+        'http://booksaz.space/s1/Anime/Ended/',
+    'http://booksaz.space/s1/Anime/OVA/',
+    'http://booksaz.space/s1/Anime/2017/',
+    'http://booksaz.space/s1/Anime/2015/',
+    'http://booksaz.space/s1/Anime/2016/',
+    'http://chise.ludost.net/Anime/'
+]
+
+@click.command(help = "It checks the given anime can be downloaded if it is, It download that :)")
+@click.option('-url', type = click.Path(), help = "Url on which anime is present", default = '')
+@click.argument('name_of_anime', type = click.Path())
+@click.pass_context
+
+
+def animename(ctx, name_of_anime, url):
+    plink = []
+    for i in urls:
+        r  = requests.get(i)
+        data = r.text
+        soup = BeautifulSoup(data, "html.parser")
+        list = soup.findAll('a', href=re.compile(name_of_anime))
+        if len(list):
+            for i in list:
+                plink.append(i.get('href'))
+    print plink
+
+
+if __name__ == '__main__':
+    animename(obj={})
+
+
+
+
+
+
+
