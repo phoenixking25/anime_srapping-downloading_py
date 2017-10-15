3d4342987268c34a39e4d0500481f459dbf5b3ff
anuj sharma <anujjangra25119@gmail.com>
2017-10-15 16:26:51 +0530

    getting download links

--- script.py
+++ script.py
@@ -8,7 +8,7 @@ urls = [
     'http://crusader.pp.ua/anime/',
     'http://booksaz.space/s1/Anime/',
     'https://archives.eyrie.org/anime/',
-        'http://booksaz.space/s1/Anime/Ended/',
+    'http://booksaz.space/s1/Anime/Ended/',
     'http://booksaz.space/s1/Anime/OVA/',
     'http://booksaz.space/s1/Anime/2017/',
     'http://booksaz.space/s1/Anime/2015/',
@@ -23,16 +23,32 @@ urls = [
 
 
 def animename(ctx, name_of_anime, url):
-    plink = []
+    dir_link = []
     for i in urls:
         r  = requests.get(i)
         data = r.text
         soup = BeautifulSoup(data, "html.parser")
         list = soup.findAll('a', href=re.compile(name_of_anime))
         if len(list):
-            for i in list:
-                plink.append(i.get('href'))
-    print plink
+            for j in list:
+                dir_link.append(i + j.get('href'))
+    if(len(dir_link)):
+        opendir(dir_link)
+    else:
+        print 'You anime not found'
+
+def opendir(dir_link):
+    print dir_link
+    download_link = []
+    for i in dir_link:
+        r = requests.get(i)
+        data = r.text
+        soup = BeautifulSoup(data, "html.parser")
+        list = soup.findAll('a', href=re.compile('.mkv'))
+        if len(list):
+            for j in list:
+                download_link.append(i + j.get('href'))
+    print download_link
 
 
 if __name__ == '__main__':
